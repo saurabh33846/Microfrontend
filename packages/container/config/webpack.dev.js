516044912b4593dc94623b5edcebe967b8f4f0e5
300071835 <saurabh.kumar1@myntra.com>
2022-03-12 17:05:12 +0530

    Created deployment workflow script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -15,9 +15,6 @@ const devConfig = {
         }
     },
     plugins:[
-        new HtmlWebpackPlugin({
-            template:'./public/index.html'
-        }),
         new ModuleFederationPlugin({
             name:'container',
             remotes:{
