e33e1d4a5233ed75d6fda2601748295ff5e3b4f8
300071835 <saurabh.kumar1@myntra.com>
2022-03-30 16:55:32 +0530

    Adde styles provider

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,13 +3,19 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header'
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName} from '@material-ui/core';
+const generateClassName = createGenerateClassName({productionPrefix:'co'})
 // Test commitee
 export default ()=>{
-    return <BrowserRouter>
+    return <BrowserRouter><StylesProvider generateClassName={generateClassName}>
+        
         <div> 
             <Header/>
             <MarketingApp/>
         </div>
+    
+    </StylesProvider>
     </BrowserRouter>
+     
     
 }
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -2,15 +2,19 @@ import React from "react";
 
 import {Switch, Route, BrowserRouter} from 'react-router-dom'
 
-import { StylesProvider } from "@material-ui/core";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 //test commit
 
+const generateClassNames = createGenerateClassName({
+    productionPrefix:'ma'
+})
+
 export default()=>{
     return <div>
-        <StylesProvider>
+        <StylesProvider generateClassName={generateClassNames}>
             <BrowserRouter>
                 <Switch>
                     <Route exact path='/pricing'>
